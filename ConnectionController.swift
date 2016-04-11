940e228d1c2489288178bbdf508ce672f98b367f
Idris Raja <idris.raja@gmail.com>
2016-04-11 10:24:00 -0500

    initial commit

---
+++ ConnectionController.swift
@@ -0,0 +1,30 @@
+//
+//  ConnectionController.swift
+//  Instagram
+//
+//  Created by id on 4/11/16.
+//  Copyright © 2016 id. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+// functionality needed:
+
+/*
+posts added
+posts deleted
+
+comments added
+comments deleted
+
+likes added
+likes deleted
+*/
+
+class ConnectionController {
+    let firebaseURL = "https://glowing-inferno-2878.firebaseio.com/"
+
+
+
+}
