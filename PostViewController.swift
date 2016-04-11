e9c6c1a73db655182f544fe98d87be31819c0e64
dp <dp@dp.local>
2016-04-11 15:08:57 -0500

    add PostViewController

---
+++ PostViewController.swift
@@ -0,0 +1,53 @@
+//
+//  PostViewController.swift
+//  Instagram
+//
+//  Created by dp on 4/11/16.
+//  Copyright © 2016 id. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+
+    }
+
+    
+    @IBAction func onImagePickerButtonTapped(sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    // MARK: - UIImagePickerControllerDelegate Methods
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .ScaleAspectFit
+            imageView.image = pickedImage
+            
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    
+}
