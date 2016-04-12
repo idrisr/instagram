b0522d9dfdef0fd5becb0cd33ce1e68996c03c32
dp <dp@dp.local>
2016-04-12 14:08:19 -0500

    image filter working

--- PostViewController.swift
+++ PostViewController.swift
@@ -9,25 +9,32 @@
 import UIKit
 import CoreImage
 
-class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
     
     let imagePicker = UIImagePickerController()
     let connectionController = ConnectionController.sharedConnection
-
+    
+    var filterNames: [String] = ["Instant", "Chrome", "Noir", "None"]
+    var filters: [String] = ["CIPhotoEffectInstant", "CIPhotoEffectChrome", "CIPhotoEffectNoir", "CIColorControls"]
+    
+    var originalImage = UIImage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         imagePicker.delegate = self
-
+        onCameraButtonTapped(self)
+        
     }
     
     func noCamera() {
         let alertVC = UIAlertController(title: "No Camera",
-                                        message: "This device does not have a camera",
+                                        message: "This device does not have a camera. Please select image from your photo gallery",
                                         preferredStyle: .Alert)
-        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: onImagePickerButtonTapped)
         alertVC.addAction(okAction)
         presentViewController(alertVC, animated: true, completion: nil)
     }
@@ -69,7 +76,7 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.contentMode = .ScaleAspectFit
             imageView.image = pickedImage
-            
+            self.originalImage = imageView.image!
         }
         
         dismissViewControllerAnimated(true, completion: nil)
@@ -80,5 +87,32 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         
     }
     
+    
+    // MARK: - TableView Delegate Methods
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.filters.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FilterCell", forIndexPath: indexPath)
+        cell.textLabel?.text = filterNames[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let CIfilterName = filters[indexPath.row]
+        print(CIfilterName)
+        
+        let context = CIContext(options: nil)
+        let startImage = CIImage(image: self.originalImage)
+        
+        let filteredImage = startImage?.imageByApplyingFilter(CIfilterName, withInputParameters: nil)
+        let renderedImage = context.createCGImage(filteredImage!, fromRect: filteredImage!.extent)
+        
+        imageView.image = UIImage(CGImage: renderedImage)
+    }
+    
+
     
 }
